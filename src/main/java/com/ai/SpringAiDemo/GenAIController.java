c3067399c8bd25b2253e8f6981dc05a53216e2b5
tomroty <tom.roty@proton.me>
2025-04-21 14:48:58 +0200

    add image generation feature

--- src/main/java/com/ai/SpringAiDemo/GenAIController.java
+++ src/main/java/com/ai/SpringAiDemo/GenAIController.java
@@ -1,27 +1,40 @@
 package com.ai.SpringAiDemo;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 
 @RestController
 public class GenAIController {
 
     private final ChatService chatService;
+    private final ImageService imageService;
 
-    public GenAIController(ChatService chatService) {
+    public GenAIController(ChatService chatService, ImageService imageService) {
         this.chatService = chatService;
+        this.imageService = imageService;
 
     }
 
     @GetMapping("ask-ai")
-    public String getResponse(@RequestParam String prompt){
+    public String getResponse(@RequestParam String prompt) {
         return chatService.getResponse(prompt);
     }
 
     @GetMapping("ask-ai-options")
-    public String getResponseOptions(@RequestParam String prompt){
+    public String getResponseOptions(@RequestParam String prompt) {
         return chatService.getResponseOptions(prompt);
+    }
+
+    @GetMapping("generate-image")
+    public void generateImages(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt);
+        String imageUrl = imageResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(imageUrl);
     }
 }
